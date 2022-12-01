1f43c65f2dcadf7f70e66d0793259ef97557ab85
heinz <mh2126@mci4me.at>
2022-12-01 15:35:21 +0100

    NEW

--- App.js
+++ App.js
@@ -75,19 +75,13 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step Zero">LEUTE LOS GEHTS!!!!</Section>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Step One">Erst mal ein Bier aufmachen !</Section>
+          <Section title="Step Two">
+            Gruppen müssen eingeteilt werden und anschließend Rollen vergeben
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
+          <Section title="Step Three">Projektplan muss erstellt werden</Section>
+          <Section title="Step Four">
+            Project-Idee muss konzepiert werden
           </Section>
           <LearnMoreLinks />
         </View>
