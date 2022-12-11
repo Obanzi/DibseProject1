7f534336b3c227ffa5e07bad499f429077da27e2
heinz <mh2126@mci4me.at>
2022-12-11 16:48:22 +0100

    NEW: Heinz

--- components/EnergyTippsComponent.js
+++ components/EnergyTippsComponent.js
@@ -17,6 +17,9 @@ import {
   useColorScheme,
   View,
   StyleSheet,
+  TouchableOpacity,
+  Text,
+  TextInput,
 } from 'react-native';
 import EnergiesparTipp_1_component from './EnergieSparComponent/EnergiesparTipp_1_component';
 import EnergiesparTipp_2_component from './EnergieSparComponent/EnergiesparTipp_2_component';
@@ -25,6 +28,10 @@ import EnergiesparTipp_4_component from './EnergieSparComponent/EnergiesparTipp_
 import EnergiesparTipp_5_component from './EnergieSparComponent/EnergiesparTipp_5_component';
 import EnergiesparTipp_6_component from './EnergieSparComponent/EnergiesparTipp_6_component';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function Spacer(props: { height: number }) {
+  return null;
+}
 
 function EnergyTipps() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -45,35 +52,104 @@ function EnergyTipps() {
           style={{
             backgroundColor: isDarkMode ? Colors.white : Colors.white,
           }}>
+          <Text style={styles.textStyleHeader}>Energiespar-Tipps</Text>
+          <Text style={styles.textStyleNormal}>
+            Energiespartipp von Heinz am 22.12.2022
+          </Text>
           <EnergiesparTipp_1_component />
+          <Text style={styles.textStyleNormal}>
+            Energiespartipp von Judith am 19.12.2022
+          </Text>
           <EnergiesparTipp_2_component />
+          <Text style={styles.textStyleNormal}>
+            Energiespartipp von Martin am 15.12.2022
+          </Text>
           <EnergiesparTipp_3_component />
+          <Text style={styles.textStyleNormal}>
+            Energiespartipp von Matthias am 12.12.2022
+          </Text>
           <EnergiesparTipp_4_component />
+          <Text style={styles.textStyleNormal}>
+            Energiespartipp von Philipp am 09.12.2022
+          </Text>
           <EnergiesparTipp_5_component />
+          <Text style={styles.textStyleNormal}>
+            Energiespartipp von Nic am 05.12.2022
+          </Text>
           <EnergiesparTipp_6_component />
-          <Button
-            style={styles.button}
-            title="Send us a message ! :)"
+          <AppButton
+            title="Schicke uns deine Energiespartipps ! :)"
             onPress={() => Linking.openURL('mailto:heimay237@gmail.com')}
           />
+        </View>
+        <View style={styles.inputView}>
+          <Spacer height={20} />
+          <TextInput
+            label={'Schicke uns deine Energiespartipps ! :)'}
+            multiline
+            inputStyle={styles.inputStyle}
+          />
+          <Spacer height={20} />
         </View>
       </ScrollView>
     </SafeAreaView>
   );
 }
 
+const AppButton = ({onPress, title}) => (
+  <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
+    <Text style={styles.appButtonText}>{title}</Text>
+  </TouchableOpacity>
+);
+
 const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
   },
-  button: {
-    backgroundColor: 'red',
-    color: 'green',
-    padding: 100,
+  appButtonContainer: {
+    elevation: 20,
+    backgroundColor: '#009688',
     borderRadius: 5,
-    fontSize: 200,
-    hight: 100,
-    width: 100,
+    paddingVertical: 10,
+    paddingHorizontal: 70,
+    marginTop: 20,
+  },
+  appButtonText: {
+    fontSize: 15,
+    color: '#fff',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    textTransform: 'uppercase',
+  },
+  textStyleHeader: {
+    fontSize: 30,
+    color: '#009688',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    textTransform: 'uppercase',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  textStyleNormal: {
+    fontSize: 13,
+    color: '#009688',
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    textTransform: 'uppercase',
+    marginTop: 20,
+    marginBottom: 5,
+  },
+  inputStyle: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    textAlignVertical: 'top',
+  },
+  inputView: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: '100%',
+    paddingTop: 20,
   },
 });
 
