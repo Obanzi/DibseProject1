cfd9d2fb978395b4686c9c5789ef8de062fe7b7b
heinz <mh2126@mci4me.at>
2022-12-15 11:58:59 +0100

    NEW_Heinz: Login wurde ergänzt

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default function App() {
           <Stack.Screen
             options={{headerShown: false}}
             name="LoginScreen"
-            component={HomeScreen}
+            component={LoginScreen}
           />
           <Stack.Screen
             options={{headerShown: false}}
